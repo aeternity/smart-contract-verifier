5971bf757b00b7c4d7ac3813a16b255fb69302a4
luekromanowicz <lromanowicz1@gmail.com>
2023-10-04 10:25:32 +0200

    fix fileName property in migration

--- apps/scv-gateway/src/database/migrations/1696407367476-UploadingContracts.ts
+++ apps/scv-gateway/src/database/migrations/1696407367476-UploadingContracts.ts
@@ -14,7 +14,7 @@ export class UploadingContracts1696407367476 implements MigrationInterface {
       `CREATE TABLE "contracts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "contract_id" character varying(63) NOT NULL, "license" character varying(127) NOT NULL, "compiler" character varying(63) NOT NULL, "entry_file" character varying NOT NULL, CONSTRAINT "PK_2c7b8f3a7b1acdd49497d83d0fb" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "contract_source_files" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "contract_id" character varying(63) NOT NULL, "file_name" character varying(127) NOT NULL, "content" character varying NOT NULL, CONSTRAINT "PK_0d6efaaf4d2908d5c38895acfc4" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "contract_source_files" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "contract_id" character varying(63) NOT NULL, "file_path" character varying(127) NOT NULL, "content" character varying NOT NULL, CONSTRAINT "PK_0d6efaaf4d2908d5c38895acfc4" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "contract_submission_source_files" ADD CONSTRAINT "FK_73efed57f0f46c53b85c22aa451" FOREIGN KEY ("submission_id") REFERENCES "contract_submissions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
