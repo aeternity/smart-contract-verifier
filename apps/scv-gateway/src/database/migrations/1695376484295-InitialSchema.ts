2332a7a3ddff964ce05c8496f33ee61c44a685ab
luekromanowicz <lromanowicz1@gmail.com>
2023-10-06 08:13:15 +0200

    fix: add uuid-ossp extension to migrations

--- apps/scv-gateway/src/database/migrations/1695376484295-InitialSchema.ts
+++ apps/scv-gateway/src/database/migrations/1695376484295-InitialSchema.ts
@@ -4,6 +4,8 @@ export class InitialSchema1695376484295 implements MigrationInterface {
   name = 'InitialSchema1695376484295';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
     await queryRunner.query(
       `CREATE TABLE "contract_submission" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "contract_id" character varying(63) NOT NULL, CONSTRAINT "PK_c94e6e55be1d22c9409d9f69c80" PRIMARY KEY ("id"))`,
     );
